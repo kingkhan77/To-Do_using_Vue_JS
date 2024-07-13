eea54e7845ff917d49788867ee34be0162703226
kingkhan77 <shahzad.2feb@gmail.com>
2024-07-13 15:26:42 +0530

    auto scroll enabled

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,26 +1,40 @@
 <script setup>
-// import { ref } from 'vue'
+import { ref, nextTick } from 'vue'
 import TodoItem from './TodoItem.vue'
 import DayAndDate from './DayAndDate.vue'
 import HeadingTitle from'./HeadingTitle.vue'
-import AddForm from './AddForm.vue'
+import TodoAddTaskForm from './TodoAddTaskForm.vue'
 import { useTodoStore } from '@/stores/useTodoStore';
 
 const todoStore = useTodoStore()
 
 const {addTask} = todoStore
 
+const listRef = ref(null)
+
+const scrollToLastTask = async() => {
+  await nextTick()
+  if (listRef.value) {
+    listRef.value.lastElementChild.scrollIntoView({ behavior: 'smooth' })
+  }
+}
+
+const handleFormSubmit = (task) => {
+  addTask(task)
+  scrollToLastTask()
+}
+
 </script>
 
 <template>
   <DayAndDate></DayAndDate>
   <HeadingTitle></HeadingTitle>
-  <ol id="list" class="list-none text-center p-[10px] pl-[40px] max-h-[73%] overflow-y-auto">
+  <ol ref="listRef" id="list" class="list-none text-center p-[10px] pl-[40px] max-h-[73%] overflow-y-auto">
     <TodoItem
       v-for="todo in todoStore.todos"
       :key="todo.id"
       :todo="todo"
     />
   </ol>
-  <AddForm  @formSubmit="addTask"></AddForm>
+  <TodoAddTaskForm  @formSubmit="handleFormSubmit"></TodoAddTaskForm>
 </template>
